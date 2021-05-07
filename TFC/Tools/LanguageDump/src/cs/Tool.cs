1f642f837561144b2e9bb589a161ad409aba5002
Dpdz Official <72412695+Devpdz@users.noreply.github.com>
2021-05-07 17:38:19 -0400

    Add files via upload

---
+++ TFC/Tools/LanguageDump/src/cs/Tool.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace Program
+{
+    class Tool
+    {
+        static void Main(string[] args)
+        {
+            GetLanguage();
+            Log("The file was created successfully.");
+        }
+        private static void Log(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        private static string Language()
+        {
+            string a = CultureInfo.CurrentCulture.EnglishName;
+            string b = a.Split('(', ')')[0b0 + 0x29A - 333 * 2];
+            return b;
+        }
+        private static void GetLanguage()
+        {
+            string[] lines =
+            {
+                "Output Info:",
+                "-- Native language of PC: "+Language(),
+                "-> Copy the line above and comment on the indicated channel.",
+                "",
+                "[#] Best regards: Thank you for trusting us! It's done :)"
+            };
+            File.WriteAllLines("InfoForMyBenefit.txt", lines);
+        }
+    }
+}
